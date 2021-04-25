a42d8b92f3880f38f16678686fbed29771aa50b3
James <jamesmca@live.com>
2021-04-25 13:26:45 -0700

    Update gatsby-config.js
    gatsby now returning title and author from siteMetadata in graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  /*localhost:8000/___graphql*/
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'James McArthur'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ],
