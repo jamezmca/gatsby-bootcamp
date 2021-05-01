e76f7e99c617871c088415802967e5eaa09d5001
James <jamesmca@live.com>
2021-05-01 12:21:49 -0700

    Update blog.js
    title data is rendered

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,23 +2,22 @@ import React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 
-// export const query = graphql`
-// query ($slug: String!){
-//   markdownRemark (fields: {slug: {eq: $slug} }) {
-//     frontmatter {
-//       title
-//       date
-//     }
-//     html
-//   }
-// }
-//`
+export const query = graphql`
+  query($slug: String) {
+    contentfulProjectPost(slug: {eq: $slug}) {
+      title
+      publishedDate(formatString:"MMMM Do, YYYY")
+    }
+  }
+`
+
 
 
 const Blog = (props) => {
     return (
         <Layout>
-          
+          <h1>{props.data.contentfulProjectPost.title}</h1>
+          <p>{props.data.contentfulProjectPost.publishedDate}</p>
         </Layout>
     )
 }
