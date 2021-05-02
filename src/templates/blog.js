b194796f3e63cdfaeeb80ba51a3c978c59d7581a
James <jamesmca@live.com>
2021-05-02 00:00:41 -0700

    Update blog.js
    somehow i managed to render the image....

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -10,18 +10,34 @@ export const query = graphql`
       publishedDate(formatString:"MMMM Do, YYYY")
       body {
         raw
+        }
+      }
+      contentfulAsset {
+        title
+        file {
+          url
+        }
+      }
+    }
+`
+const Blog = (props) => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = props.data.contentfulAsset.title
+        const url = props.data.contentfulAsset.file.url
+        return <img alt={alt} src={url} />
       }
     }
   }
-`
-const Blog = (props) => {
-    return (
-        <Layout>
-          <h1>{props.data.contentfulProjectPost.title}</h1>
-          <p>{props.data.contentfulProjectPost.publishedDate}</p>
-          {documentToReactComponents(JSON.parse(props.data.contentfulProjectPost.body.raw))}
-        </Layout>
-    )
+
+  return (
+    <Layout>
+      <h1>{props.data.contentfulProjectPost.title}</h1>
+      <p>{props.data.contentfulProjectPost.publishedDate}</p>
+      {documentToReactComponents(JSON.parse(props.data.contentfulProjectPost.body.raw), options)}
+    </Layout>
+  )
 }
 
 export default Blog
